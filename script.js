685c8d17a485b8acf28f557a0fc21f81d0bae8c4
Yash Gautam <98219582+iTrydant@users.noreply.github.com>
2022-01-29 13:47:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,4 @@
 const number = Number(
   prompt("Enter the number you want to find the square of: ")
 );
-
-switch (number) {
-  default:
-    document.write("Please enter an number!");
-    break;
-}
-
 document.write(`The square of ${number} is ${number * number}`);
